02aec9aa7627736330c97e558e51f26c8de92506
Bohdan <akopyan.bogdan@gmail.com>
2018-06-13 13:37:39 +0300

    Add field and properties to TreeItem class

--- JSON Tree/JSON Tree/Program.cs
+++ JSON Tree/JSON Tree/Program.cs
@@ -5,10 +5,19 @@ using System.IO;
 
 namespace JSON_Tree
 {
+    class TreeItem
+    {
+        public string Name { get; set; }
+        public string DateCreated { get; set; }
+        public string Path { get; set; }
+        public string Size { get; set; }
+        public List<TreeItem> Files;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
